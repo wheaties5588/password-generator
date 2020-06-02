405353c91b1f859d3ea85e6ea1396b6dfb821201
Matt Wieciech <matt.wieciech@gmail.com>
2020-06-02 18:27:50 -0400

    HTML setup with styling and JS pseudo

---
+++ app.js
@@ -0,0 +1,16 @@
+//Grab elements needed - button, output field
+
+
+
+//Create arrays of the different options of characters allowed in password
+
+//Alphabetical characters
+
+//Numbers
+
+//Special Characters
+
+
+
+
+//Prompt for length between 8 and 128 characters
